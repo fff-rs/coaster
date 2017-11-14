c039be8f8232d53ae48d08116a16be48d4e1f21a
Sir Mergealot <sirmergealot@spearow.io>
2017-11-14 21:41:23 +0000

    doc/automatic: update

---
+++ implementors/core/ops/bit/trait.BitOr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["coaster"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"coaster/frameworks/opencl/memory/struct.MemoryFlags.html\" title=\"struct coaster::frameworks::opencl::memory::MemoryFlags\">MemoryFlags</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"coaster/frameworks/opencl/queue/struct.QueueFlags.html\" title=\"struct coaster::frameworks::opencl::queue::QueueFlags\">QueueFlags</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["coaster"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"coaster/frameworks/native/flatbox/struct.FlatBox.html\" title=\"struct coaster::frameworks::native::flatbox::FlatBox\">FlatBox</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"coaster/frameworks/opencl/memory/struct.Memory.html\" title=\"struct coaster::frameworks::opencl::memory::Memory\">Memory</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"coaster/frameworks/cuda/context/struct.Context.html\" title=\"struct coaster::frameworks::cuda::context::Context\">Context</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"coaster/frameworks/cuda/memory/struct.Memory.html\" title=\"struct coaster::frameworks::cuda::memory::Memory\">Memory</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
